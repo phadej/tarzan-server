237da904eb36067c1f7af6f17ab76e81518dd206
Oleg Grenrus <oleg.grenrus@iki.fi>
2014-09-05 11:09:59 +0300

    Read port from environment

--- webapp/Web.hs
+++ webapp/Web.hs
@@ -1,6 +1,9 @@
 module Main where
 
+import Control.Applicative
+
 import Data.String (fromString)
+import System.Environment (lookupEnv)
 
 import Network.Wai.Handler.Warp
 
@@ -9,13 +12,14 @@ import Application (tarzanApplication)
 host :: String
 host = "127.0.0.1"
 
-port :: Int
-port = 8080
+portIO :: IO Int
+portIO = maybe 8080 read <$> lookupEnv "PORT"
 
-settings :: Settings
-settings = setPort port $ setHost (fromString host) defaultSettings
+settings :: Int -> Settings
+settings port = setPort port $ setHost (fromString host) defaultSettings
 
 main :: IO ()
 main = do
+	port <- portIO
 	putStrLn $ "http://" ++ host ++ ":" ++ show port
-	runSettings settings tarzanApplication
+	runSettings (settings port) tarzanApplication
